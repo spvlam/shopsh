e4bf7fdad558d0c9be0480fcace714c8c7d73a0e
unknown <you@example.com>
2022-12-08 15:21:23 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,15 @@ const courseRoute = require('./course')
 const meRoute = require('./me');
 const LogInRoute = require('./logIn');
 function routes(app){
+    
     app.use('/home',siteRoute);
     app.use('/course',courseRoute);
     app.use('/news',newRoute);
     app.use('/me',meRoute);
     app.use('/login',LogInRoute);
-
+    app.use('/',function(req,res){
+        res.send('test');
+    })
     // app.get('/news',function(req,res){
     //     res.render('news');
     //     console.log(req.query.password)
