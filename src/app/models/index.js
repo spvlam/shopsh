47c28e09da95c8aad992823f978553ecf1a89376
unknown <you@example.com>
2022-12-08 22:03:37 +0700

    done

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -3,14 +3,15 @@ const mongoose= require('mongoose')
 //connecting to mongDB, especially connect to f8_education data
 
 async function connect(){
-    // try {
-    //     await mongoose.connect('mongodb+srv://vercel-admin-user:vercel-admin-user@cluster0.ubjvpxo.mongodb.net/f8_education')
-    //     console.log("connect sucessfully !!!")
-    // } catch (error) {
-    //     if(error){
-    //         console.log("connect failed !!!")
-    //     }
-    // }
+    console.log('abc')
+    try {
+        await mongoose.connect('mongodb+srv://vercel-admin-user:vercel-admin-user@cluster0.ubjvpxo.mongodb.net/f8_education')
+        console.log("connect sucessfully abc !!!")
+    } catch (error) {
+        if(error){
+            console.log("connect failed !!!")
+        }
+    }
 }
 // why we need to export connect 
 module.exports ={connect} 
